4f326136d0b4c7b79a72bcaf11ae3b1c02f47233
LaMaquinaMVeloz <onltrabalho@gmail.com>
2024-09-27 16:44:18 -0300

    this

--- src/pages/StartScreen/Styles.ts
+++ src/pages/StartScreen/Styles.ts
@@ -1,55 +1,55 @@
-import styled from "styled-components"
+// import styled from "styled-components"
 
-export const PageContainer = styled.div`
-  background-color: #3e3e3e;
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
+// export const PageContainer = styled.div`
+//   background-color: #3e3e3e;
+//   height: 100vh;
+//   width: 100vw;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `
 
-export const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 100px;
-`
+// export const ButtonContainer = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   gap: 100px;
+// `
 
-export const Button = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: white;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  padding: 20px;
-  width: 100px;
-  cursor: pointer;
-  transition: transform 0.2s;
-  background-color: #d3d3d3;
+// export const Button = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   background-color: white;
+//   border: 1px solid #ccc;
+//   border-radius: 8px;
+//   padding: 20px;
+//   width: 100px;
+//   cursor: pointer;
+//   transition: transform 0.2s;
+//   background-color: #d3d3d3;
 
-  &:hover {
-    transform: scale(1.05);
-  }
-`
+//   &:hover {
+//     transform: scale(1.05);
+//   }
+// `
 
-export const ProfileContainer = styled.div`
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  background-color: #333;
-  color: white;
-  border-radius: 10px;
-  padding: 15px 40px;
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  font-size: 1.1em;
-`
+// export const ProfileContainer = styled.div`
+//   position: absolute;
+//   top: 20px;
+//   right: 20px;
+//   background-color: #333;
+//   color: white;
+//   border-radius: 10px;
+//   padding: 15px 40px;
+//   display: flex;
+//   align-items: center;
+//   gap: 10px;
+//   font-size: 1.1em;
+// `
 
-export const ProfileImage = styled.img`
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-`
+// export const ProfileImage = styled.img`
+//   width: 40px;
+//   height: 40px;
+//   border-radius: 50%;
+// `
