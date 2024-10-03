9406f9a3d38aa84eb49db8329f1074e091586e49
LaMaquinaMVeloz <onltrabalho@gmail.com>
2024-10-03 15:21:35 -0300

    receba

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -4,13 +4,14 @@ import Bloom from "../../assets/icons/search.svg";
 export interface InputProps {
     placeholder?: string;
     type?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function Search({ placeholder, type }: InputProps) {
+export default function Search({ placeholder, type, onChange }: InputProps) {
     return (
         <InputContainer>
             <Icon src={Bloom} alt="Bloom" />
-            <InputStyled placeholder={placeholder} type={type} />
+            <InputStyled placeholder={placeholder} type={type} onChange={onChange} />
         </InputContainer>
     );
 }
